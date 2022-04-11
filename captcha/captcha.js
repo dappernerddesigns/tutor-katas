aa54057dbe95fcc9d1cc1e37738f61497fedc9b2
Verity Gregory <verity.gregory@gmail.com>
2022-04-11 10:20:15 +0100

    solution for captcha challenge complete

--- captcha/captcha.js
+++ captcha/captcha.js
@@ -1,5 +1,17 @@
 const input = require('./input');
 
-function captchaSum(num) {}
+function captchaSum(num) {
+	const stringify = num.toString();
+	//modify number input so that first number and last number can be compared
+	const modString = stringify + stringify[0];
 
+	let sum = 0;
+	for (let i = 0; i < modString.length; i++) {
+		if (modString[i] === modString[i + 1]) {
+			sum += Number(modString[i]);
+		}
+	}
+	return sum;
+}
+console.log(captchaSum(input));
 module.exports = captchaSum;
--- captcha/captcha.test.js
+++ captcha/captcha.test.js
@@ -2,5 +2,37 @@ const input = require('./input');
 const captchaSum = require('./captcha');
 
 describe('Captcha function', () => {
-	test('');
+	test('Function returns a number', () => {
+		const actual = captchaSum(1122);
+
+		expect(typeof actual).toBe('number');
+	});
+	test('Function returns zero for no matching integers', () => {
+		const actual = captchaSum(123);
+		const expected = 0;
+
+		expect(actual).toBe(expected);
+	});
+	test('Function correctly sums small number', () => {
+		const actual = captchaSum(1123);
+		const expected = 1;
+
+		expect(actual).toBe(expected);
+	});
+	test('Function correctly sums more complex arrangement', () => {
+		const actual = captchaSum(1122);
+		const expected = 3;
+
+		expect(actual).toBe(expected);
+		expect(captchaSum(1111)).toBe(4);
+		expect(captchaSum(1234)).toBe(0);
+		expect(captchaSum(91212129)).toBe(9);
+	});
+	test('Breaking down larger input to test function', () => {
+		const biggerTest = '31813174349235972159811869755166343';
+		const actual = captchaSum(biggerTest);
+		const expected = 15;
+
+		expect(actual).toBe(expected);
+	});
 });
